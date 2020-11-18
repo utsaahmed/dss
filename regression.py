b3e5f84022c160c8b3a34eaaf32324db065304b3
Utsa Ahmed <74656185+utsaahmed@users.noreply.github.com>
2020-11-19 00:23:47 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -9,16 +9,31 @@ TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
 
 
-def predict_price(area) -> float:
+ef predict_price(area) -> float:
     """
     This method must accept as input an array `area` (represents a list of areas sizes in sq feet) and must return the respective predicted prices (price per sq foot) using the linear regression model that you build.
-
     You can run this program from the command line using `python3 regression.py`.
     """
     response = requests.get(TRAIN_DATA_URL)
+    decoded_content = response.content.decode('utf-8')
+
+    df = csv.reader(decoded_content.splitlines(), delimiter=',')
+    df=list(df)
+    ar_train=list(map(float,df[0][1:]))
+    pr_train=list(map(float,df[1][1:]))
+    ar_train=numpy.array(ar_train)
+    pr_train=numpy.array(pr_train)
     # YOUR IMPLEMENTATION HERE
     ...
-
+    
+    
+    regre=LinearRegression()
+    regre.fit(ar_train.reshape(-1,1),pr_train.reshape(-1,1))
+    pr_pred=regre.predict(areas.reshape(-1,1))
+    print(regre.intercept_)
+    print(regre.coef_)
+    
+    return pr_pred
 
 if __name__ == "__main__":
     # DO NOT CHANGE THE FOLLOWING CODE
